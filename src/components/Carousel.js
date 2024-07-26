a072059f72500482639d560a064cba23450a1c3d
Asen Volev <asen.volev@egt-digital.com>
2024-07-26 09:16:53 +0300

    use percentages and indexes so to be responsible

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -9,6 +9,7 @@ const Carousel = ({imageUrls, imagesToShiftCount}) => {
     const [translateX, setTranslateX] = useState(-window.innerWidth * imagesToShiftCount);
     const [initialTranslateX, setInitialTranslateX] = useState(0);
     const [transitionEnabled, setTransitionEnabled] = useState(true);
+    const [currentIndex, setCurrentIndex] = useState(imagesToShiftCount);
     const [imageIndexes, setImageIndexes] = useState(Array.from({ length: 2*imagesToShiftCount+1 }, (_, index) => index));
 
 
@@ -20,64 +21,51 @@ const Carousel = ({imageUrls, imagesToShiftCount}) => {
                     e.preventDefault();
                 }
             };
-            const onResize = throttle(() => setTranslateXWithoutTransition(-window.innerWidth * imagesToShiftCount), 10);
+            // const onResize = throttle(() => setCurrentIndexWithoutTransition(imagesToShiftCount), 10);
 
 
             carousel.addEventListener('wheel', onWheel);
             carousel.addEventListener("keydown", preventArrowKeyScroll);
-            window.addEventListener('resize', onResize);
+            // window.addEventListener('resize', onResize);
             return () => {
                 carousel.removeEventListener("keydown", preventArrowKeyScroll);
                 carousel.removeEventListener('wheel', onWheel);
-                window.removeEventListener('resize', onResize);
+                // window.removeEventListener('resize', onResize);
             };
         }
     }, []);
 
 
     const addAndRemoveImage = () => {
-        const carousel = carouselRef.current;
-        if (carousel) {
-            const slideWidth = window.innerWidth;
-            const currentSlide = Math.round(translateX / -slideWidth);
-            if (currentSlide < 2) {
-                setImageIndexes(prevImages => {
-                    const newImages = [...Array.from({ length: imagesToShiftCount }, (_, i) => prevImages[0] - (i + 1)).reverse(), ...prevImages.slice(0, -imagesToShiftCount)];
-                    setTranslateXWithoutTransition(-slideWidth * (imagesToShiftCount+1));
-                    return newImages;
-                });
-            } else if (currentSlide > (imagesToShiftCount * 2 - 1)) {
-                setImageIndexes(prevImages => {
-                    const newImages = [...prevImages.slice(imagesToShiftCount), ...Array.from({ length: imagesToShiftCount }, (_, i) => prevImages[prevImages.length - 1] + (i + 1))];
-                    setTranslateXWithoutTransition(-slideWidth * imagesToShiftCount);
-                    return newImages;
-                });
-            }
+        if (currentIndex < 2) {
+            setImageIndexes(prevImages => {
+                const newImages = [...Array.from({ length: imagesToShiftCount }, (_, i) => prevImages[0] - (i + 1)).reverse(), ...prevImages.slice(0, -imagesToShiftCount)];
+                setCurrentIndexWithoutTransition(imagesToShiftCount+1);
+                return newImages;
+            });
+        } else if (currentIndex > (imagesToShiftCount * 2 - 1)) {
+            setImageIndexes(prevImages => {
+                const newImages = [...prevImages.slice(imagesToShiftCount), ...Array.from({ length: imagesToShiftCount }, (_, i) => prevImages[prevImages.length - 1] + (i + 1))];
+                setCurrentIndexWithoutTransition(imagesToShiftCount);
+                return newImages;
+            });
         }
     };
 
-    const setTranslateXWithoutTransition = (value) => {
+    const setCurrentIndexWithoutTransition = (value) => {
         setTransitionEnabled(false);
-        setTranslateX(value);
+        setCurrentIndex(value);
         requestAnimationFrame(() => {
             setTransitionEnabled(true);
         });
     };
 
     const moveToNextSlide = (delta) => {
-        const slideWidth = window.innerWidth;
-        setTranslateX((prevTranslateX) => {
-            const currentSlide = Math.round(prevTranslateX / -slideWidth);
-            let newTranslateX;
-            if (Math.abs(delta) > slideWidth / 2) {
-                newTranslateX = currentSlide * -slideWidth;
-            } else if (delta < 0) {
-                newTranslateX = (currentSlide + 1) * -slideWidth;
-            } else if (delta > 0) {
-                newTranslateX = (currentSlide - 1) * -slideWidth;
-            }
-            return newTranslateX;
-        });
+        if (delta < 0) {
+            setCurrentIndex(prevIndex => prevIndex+1);
+        } else if (delta > 0) {
+            setCurrentIndex(prevIndex => prevIndex-1);
+        }
     };
 
     const onWheel = throttle((e) => {
@@ -116,7 +104,7 @@ const Carousel = ({imageUrls, imagesToShiftCount}) => {
                 onTouchStart={onTouchStart}
                 onTouchMove={onTouchMove}
                 onTouchEnd={onTouchEnd}
-                style={{ transform: `translateX(${translateX}px)`, transition: transitionEnabled ? 'transform 0.3s ease' : 'none' }}
+                style={{ transform: `translateX(-${currentIndex*100}%)`, transition: transitionEnabled ? 'transform 0.3s ease' : 'none' }}
                 onTransitionEnd={onTransitionEnd}
             >
                 {imageIndexes.map((val, index) => {
